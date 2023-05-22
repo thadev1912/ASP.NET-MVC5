234b0fbe88418181d53ecc20ab2a026417774f5d
thadev <thachchanhtha1912@gmail.com>
2023-05-22 11:12:12 +0700

    Add Demo

--- Quanlykhohang/Models/Model1.Designer.cs
+++ Quanlykhohang/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\ASPNET\Quanlykhohang\Quanlykhohang\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Source\Repos\ASP.NET-MVC567\Quanlykhohang\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
